fd28b680d20bd1f5884f5039f09d17c84363bc2a
Abhineet Sharma <sharma.abhineet31@gmail.com>
2025-04-10 03:57:06 -0400

    with more organized terraform setup
    - Move all files to terraform directory
    Plan:
    - Terraform should be able to create or tap into existing cloudfront setup
    - Create/Use existing certificate
    - Create proper routes in Route53

---
+++ terraform/locals.tf
@@ -0,0 +1,17 @@
+locals {
+  content_types = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "text/javascript",
+    ".png": "image/png",
+    ".ico": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".map": "application/json",
+    ".eot": "application/vnd.ms-fontobject",
+    ".woff": "font/woff",
+    ".woff2": "font/woff2",
+    ".svg": "image/svg+xml",
+    ".pdf": "application/pdf"
+  }
+}
---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "module_path" {
+  value = path.module
+}
+
+output "root_path" {
+  value = path.root
+}
+
+output "cwd_path" {
+  value = path.cwd
+}
